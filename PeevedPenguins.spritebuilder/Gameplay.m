fd92e0bfd123d13b117dd59b68bb8fe8829f096e
darkverbito <gcermak@iMac.local>
2014-07-06 12:28:56 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -22,6 +22,8 @@
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    _physicsNode.debugDraw = TRUE;
 }
 
 // called on every touch in this scene
